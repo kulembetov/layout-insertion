50c494202530d4369f628f2a142d38643be791fd
vvvadim <v.vladimirov@it-park.tech>
2025-07-22 18:50:06 +0300

    chore: placeholder

---
+++ figma_api/api_v1/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+class FigmaFilterConfigSerializer(serializers.ModelSerializer):
+    class Meta:
+        ...
+
+class ExtractedBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        ...
+
+class ExtractedSlideSerializer(serializers.ModelSerializer):
+    class Meta:
+        ...
+
+class FigmaResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        ...
