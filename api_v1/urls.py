c89f1839993ad5e4bf2a3eba5dff16886aac8b20
vvvadim <v.vladimirov@it-park.tech>
2025-07-24 17:57:36 +0300

    chore: commit url [wip]

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from api_v1.views_ver_2 import APIReceiveJsonFromFigma
 from api_v1.views import ReceiveFigmaJsonAPIView
 
 
 urlpatterns = [
-    path('figma/extract/', APIReceiveJsonFromFigma.as_view()),
     path('figma/extract/wip/', ReceiveFigmaJsonAPIView.as_view()),
 ]
