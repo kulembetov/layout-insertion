806232f4aed2669285439e048dcc320cf84f9070
vvvadim <v.vladimirov@it-park.tech>
2025-07-29 11:39:40 +0300

    refactor: redis utils

--- api_v1/redis/utils.py
+++ api_v1/redis/utils.py
@@ -5,20 +5,21 @@ from config.settings import CACHE_ENABLED
 
 
 def gen_key(file_id: str, filter_mode: Optional[str] = None, params: Optional[list] = None) -> str:
-    key = f"figma:{file_id}"
-    if filter_mode is not None:
+    key = f"figma={file_id}"
+    if filter_mode and params:
         key += f":filter-mode={filter_mode}"
-        
-        if params is not None:
-            param_str = ':'.join(map(str, sorted(params)))
-            key += f":params={param_str}"
+        param_str = ':'.join(map(str, sorted(params)))
+        key += f":params={param_str}"
     return key
 
+
 def get_cached_request(key: str) -> Optional[dict]:
+    # to-do: refactor
     if CACHE_ENABLED and cache.get(key):
         return cache.get(key)
     return None
 
+
 def set_cached_request(key: str, data: dict) -> None:
-    if CACHE_ENABLED and not cache.get(key):
+    if CACHE_ENABLED:
         cache.set(key, data)
