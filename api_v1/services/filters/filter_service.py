3655a608069c797aa31bc7696d928fd7142985b0
vvvadim <v.vladimirov@it-park.tech>
2025-07-29 13:41:22 +0300

    chore: improve FilterFigmaAPI

--- api_v1/services/filters/filter_service.py
+++ api_v1/services/filters/filter_service.py
@@ -1,49 +1,41 @@
 from typing import Any, Optional, Dict
 
 from .filter_settings import FilterMode, FilterConfig
-from ..figma_api import FigmaAPI
+from api_v1.services.figma_api import FigmaAPI
 
 
-class FilterFigmaApi:
+class FilterFigmaApi(FigmaAPI):
     """Use different types of filtration."""
 
-    def __init__(self, file_id: str, token: Optional[str] = None, filter_params: list = None) -> None:
-        self.token = token
+    def __init__(self, file_id: str, token: Optional[str] = None, filter_params: Optional[list] = None) -> None:
+        super().__init__(file_id=file_id, token=token)
         self.filter_params = filter_params
-        self.file_id = file_id
 
     def extract_specific_slides(self) -> Dict[str, Any]:
         """Extract specific slides from Figma."""
 
-        filter_config = FilterConfig(
+        self.filter_config = FilterConfig(
             mode=FilterMode.SPECIFIC_SLIDES,
             target_slides=self.filter_params,
             require_z_index=True
         )
-        return self.get_figma_api_instance(filter_config=filter_config).extract()
+        return self.extract()
     
 
     def extract_specific_blocks(self) -> Dict[str, Any]:
         """Extract slides containing specific block types."""
 
-        filter_config = FilterConfig(
+        self.filter_config = FilterConfig(
             mode=FilterMode.SPECIFIC_BLOCKS,
             target_block_types=self.filter_params,
         )
-        return self.get_figma_api_instance(filter_config=filter_config).extract()
+        return self.extract()
     
     def extract_by_type(self) -> Dict[str, Any]:
         """Extract slides from specific containers"""
 
-        filter_config = FilterConfig(
+        self.filter_config = FilterConfig(
             mode=FilterMode.BY_TYPE,
             target_containers=self.filter_params
         )
-        return self.get_figma_api_instance(filter_config=filter_config).extract()
-    
-    def get_figma_api_instance(self, filter_config) -> 'FigmaAPI':
-        """Get FigmaAPI instance."""
-        figma = FigmaAPI(token=self.token)
-        figma.file_id = self.file_id
-        figma.filter_config = filter_config
-        return figma
+        return self.extract()
