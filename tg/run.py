930cbb2dc0595f2e3b3efa285a6334d8a5200ca8
vvvadim <v.vladimirov@it-park.tech>
2025-08-01 14:08:27 +0300

    chore: include start_router

--- tg/run.py
+++ tg/run.py
@@ -1,6 +1,7 @@
-"""This file is intended to collect the entire project and run it"""
-
+"""This file is intended to collect the entire tg project and run it"""
 import asyncio
+
+from handlers.commands.start import start_router
 
 from loader import bot, dp
 from log_utils import setup_logger
@@ -9,7 +10,7 @@ from log_utils import setup_logger
 logger = setup_logger(__name__)
 
 async def run_main():
-    # include routers there
+    dp.include_router(start_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     logger.info('Starting a bot...')
