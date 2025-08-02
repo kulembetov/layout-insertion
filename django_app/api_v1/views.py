e41ff4d7da5d7f1e9c45948e4fd77acad29646de
vvvadim <v.vladimirov@it-park.tech>
2025-08-02 14:57:52 +0300

    chore: update views.py

--- django_app/api_v1/views.py
+++ django_app/api_v1/views.py
@@ -3,42 +3,57 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django_app.api_v1.services.filters.filter_settings import FilterMode
-from django_app.api_v1.implemented import figma_instance
-from django_app.api_v1.implemented import filter_figma_instance
 from django_app.api_v1.utils.helpers import json_dump
 
 from log_utils import setup_logger, logs
 
 
-
 logger = setup_logger(__name__)
 
-
 class ReceiveFigmaJsonAPIView(APIView):
-    """Receive Data From Figma."""
+    """Receive data from Figma."""
 
     @logs(logger, on=True)
     def get(self, request):
-        file_id = request.data['file_id']
+        from .implemented import figma_instance
+
+        try:
+            file_id = request.data['file_id']
+            logger.info(f'file_id: {file_id}')
+        except KeyError:
+            return Response(
+                data={'message': "Request doesn't contain 'file_id'. Bad request."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         figma_instance.file_id = file_id
-        logger.info(f'file_id: {file_id}')
-        
         data = figma_instance.extract()
+
         json_dump(data, 'output.json')
         return Response(data=data, status=status.HTTP_200_OK)
     
 
 class FilterFigmaJson(APIView):
-    """Filter Data Recieved From Figma And Add It Into DB."""
+    """Filter data received from Figma and add it into database."""
 
     @logs(logger, on=True)
     def get(self, request):
+        try:
+            file_id = request.data['file_id']
+            logger.info(f'file_id: {file_id}')
+        except KeyError:
+            return Response(
+                data={'message': "Request doesn't contain 'file_id'. Bad request."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         if request.data.get('filter'):
+            from .implemented import filter_figma_instance
 
             filter_type: str = request.data.get('filter').get('type')
             filter_names: list[str] = request.data.get('filter').get('name')
 
-            filter_figma_instance.file_id = request.data['file_id']
+            filter_figma_instance.file_id = file_id
             filter_figma_instance.filter_names = filter_names
 
             match filter_type:
@@ -57,4 +72,7 @@ class FilterFigmaJson(APIView):
             return Response(data=data, status=status.HTTP_200_OK)
 
 
-        return Response(data = {'message': "Request doesn't contain 'filter'. Bad request."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={'message': "Request doesn't contain 'filter'. Bad request."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
