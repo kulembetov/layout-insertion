ffe12e717ffe79356b604361397f66b96dfd673f
Nikita <NikTihomirov@yandex.ru>
2025-08-05 13:03:46 +0300

    feat: create app for working in db

---
+++ db_work/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+from typing import Optional
+from sqlalchemy.engine.base import Engine
+
+load_dotenv()
+
+def create_connection() -> Optional["Engine"]:
+    """Create connection to DB."""
+
+    DB_NAME = os.getenv("POSTGRES_DB")
+    USERNAME = os.getenv("POSTGRES_USER")
+    PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    HOST = os.getenv("POSTGRES_HOST")
+    PORT = os.getenv("POSTGRES_PORT")
+    DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+
+    try:
+        engine = create_engine(DATABASE_URL, echo=False)
+        return engine
+    except Exception:
+        raise
