de2d81fb10018b5d6254db2f91f317df43060d5b
vvvadim <v.vladimirov@it-park.tech>
2025-08-06 13:46:55 +0300

    feat: insert layout in db

--- tg/handlers/sql_insert.py
+++ tg/handlers/sql_insert.py
@@ -2,24 +2,27 @@ from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from db_work.services import PresentationLayoutManager
 from log_utils import setup_logger
 from tg.states import InsertingState
-from tg.utils import generate_uuid
+from tg.utils import to_str_user
 
 insert_router = Router()
+manager = PresentationLayoutManager()
 
 logger = setup_logger(__name__)
 
 
 @insert_router.message(F.text, InsertingState.name)
 async def insert_logic(message: Message, state: FSMContext):
-    # wait for check func
-    # check if layout with this name already exists or not
-    # start 'update_logic' if it's True
-    # W.I.P
+    name = message.text
+    is_exists: bool | None = manager.select_an_entry_from_presentation_layout(name)
+    if is_exists:
+        await message.answer("Такой шаблон уже существует. Запуск сценария 'update'.")
+        await state.clear()
 
-    uid = generate_uuid()
-    await state.update_data(name=message.text, uid=uid)
+    else:
+        manager.insert_an_entry_in_presentation_layout(name)
 
-    logger.info(f"name={message.text}, uid={uid}")
-    # do request to db and create new row with name and id
+        str_user = to_str_user(message.from_user)
+        logger.info(f"Пользователь {str_user} добавил шаблон с именем {name}.")
