90a1fa7597f9195491105312aea1197522c56228
vvvadim <v.vladimirov@it-park.tech>
2025-08-08 16:26:11 +0300

    feat: realize parsing figma url and extracting file id

--- tg/handlers/layout_loading.py
+++ tg/handlers/layout_loading.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 
 from log_utils import setup_logger
 from tg.states import LayoutLoadingState, OptionState
-from tg.utils import to_str_user
+from tg.utils import extract_file_id, is_valid_figma_url, to_str_user
 
 layout_loading_router = Router()
 
@@ -16,8 +16,23 @@ async def layout_loading(message: Message, state: FSMContext) -> None:
     from tg.markups import start_markup
 
     figma_url_raw = message.text
-    str_user = to_str_user(message.from_user)
-    logger.info(f'Пользователь {str_user} якобы загрузил шаблон "{figma_url_raw}".')
+    if is_valid_figma_url(figma_url_raw):
+        file_id = extract_file_id(figma_url_raw)
+        if file_id:
+            logger.info(f"Extracted file id: {file_id}")
 
-    await message.answer("Выберите опцию:", reply_markup=start_markup.get())
-    await state.set_state(OptionState.choosing)
+            # call request to django app
+
+            str_user = to_str_user(message.from_user)
+            logger.info(f"Пользователь {str_user} якобы загрузил шаблон '{figma_url_raw}'.")
+
+            await message.answer("Выберите опцию:", reply_markup=start_markup.get())
+            await state.set_state(OptionState.choosing)
+
+        else:
+            logger.error(f"file_id={file_id}, raw_url={figma_url_raw}")
+            await message.answer("Некорректный 'file_id'. Попробуйте загрузить ссылку еще раз.")
+            await state.set_state(LayoutLoadingState.loading)
+    else:
+        await message.answer("Неверная ссылка. Попробуйте загрузить ссылку еще раз.")
+        await state.set_state(LayoutLoadingState.loading)
--- tg/utils.py
+++ tg/utils.py
@@ -1,3 +1,6 @@
+import re
+from urllib.parse import urlparse
+
 from aiogram.types import User
 
 from tg.settings import ADMIN_LIST
@@ -13,3 +16,41 @@ def to_str_user(user: User) -> str:
     if user.username:
         return f"@{user.username}"
     return f"ID: {user.id}"
+
+
+def is_valid_figma_url(url_raw: str) -> bool:
+    try:
+        u = urlparse(url_raw)
+    except Exception:
+        return False
+
+    if u.scheme != "https":
+        return False
+
+    if u.netloc not in {"www.figma.com", "figma.com"}:
+        return False
+
+    figma_path = re.compile(
+        r"""
+    ^design/                            # первый сегмент
+    (?P<file_id>[A-Za-z0-9]+)/          # file_id — безопасные символы
+    (?P<name>[^/?#]+)$                  # name — один сегмент без / ? #
+    """,
+        re.X,
+    )
+
+    # оставляем только первые три сегмента пути
+    path = "/".join(u.path.strip("/").split("/")[:3])
+    if not figma_path.match(path):
+        return False
+
+    # query может быть пустым или любым (param1=...&param2=...)
+    # фрагмент (#...) тоже допускается
+    return True
+
+
+def extract_file_id(url: str) -> str | None:
+    u = urlparse(url)
+    m = re.search(r"/design/([^/]+)(?=/|$)", u.path)
+    file_id = m.group(1) if m else None
+    return file_id
