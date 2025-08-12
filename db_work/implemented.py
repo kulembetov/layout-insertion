11d90b293b9c6b85b94aef54d8ffa1d4c6184801
vvvadim <v.vladimirov@it-park.tech>
2025-08-12 16:03:15 +0300

    chore: implement db entities

---
+++ db_work/implemented.py
@@ -0,0 +1,6 @@
+from db_work.services import ColorSettingsManager, PresentationLayoutManager, PresentationLayoutStylesManager, SlideLayoutManager
+
+presentation_layout_manager = PresentationLayoutManager()
+color_settings_manager = ColorSettingsManager()
+presentation_layout_styles_manager = PresentationLayoutStylesManager()
+slide_layout_manager = SlideLayoutManager()
